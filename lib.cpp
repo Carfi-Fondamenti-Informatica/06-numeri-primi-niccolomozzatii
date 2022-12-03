d63d19e094ab3de35b4af34853194f509ca62c2d
niccolomozzatii <118431871+niccolomozzatii@users.noreply.github.com>
2022-12-03 01:33:23 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
-
+int primo (int n, int m) {
+  bool x=true;
+  if(m!=1){
+    if((n % m) != 0){
+      m--;
+      x= primo(n, m);
+    } else {
+      x = false;
+    }
+  } else {
+    x = true;
+  } return x;
+}
