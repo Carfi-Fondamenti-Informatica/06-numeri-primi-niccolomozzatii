82284aeb754ee5ebe780dfb66ff31c41ea289a4f
niccolomozzatii <118431871+niccolomozzatii@users.noreply.github.com>
2022-12-03 01:38:15 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
-
+#include "lib.h"
 int main(){
+  int n=0; cin >> n;
+  int m=(n-1);
+  if (primo (n;m) == true) {
+    cout << "numero primo";
+  } else if (primo (n, m) == false) {
+    cout << "numero non primo";
+  }
   return 0;
 }
